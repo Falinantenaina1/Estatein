26fa71239940caca1da25a35282fa63b3ed37953
Falinantenaina RANAIVOJAONA <falinantenainaranaivojaona@gmail.com>
2024-07-30 14:31:43 +0300

    Ajout de la navigation de la page

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+const menuButton = document.getElementById('menu-button');
+const navbar = document.getElementById('navbar');
+
+menuButton.addEventListener('click', () => {
+    navbar.classList.toggle("show");
+})
+
+navbar.addEventListener('click', () => {
+    navbar.classList.remove("show");
+})
